e98239037f5fb19bd90a79c2c518b8719f0f07e6
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-07-24 14:53:47 -0300

    header e menu

--- news-website/src/app/header/header.component.ts
+++ news-website/src/app/header/header.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  isMenuOpen: boolean = false;
 
-}
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }}
